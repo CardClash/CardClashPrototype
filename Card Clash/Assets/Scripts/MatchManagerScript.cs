1edcab9c114348ab8cf43f17f50227033405ce19
mjp8835 <mjp8835@rit.edu>
2018-09-03 11:25:07 -0400

    Uploaded my old starting project
    This is in Unity 2018.2.5f1 (64 bit). Also, the MatchManagerScript is pretty useless. I would recommend scrapping it and starting from scratch.

---
+++ Card Clash/Assets/Scripts/MatchManagerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum GamePhase { CardBegin, CardMainOne, CardCombat, CardMainTwo, CardEnd, SkirmishBegin, SkirmishMain, SkirmishEnd };
+
+public class MatchManagerScript : MonoBehaviour
+{
+    public int battleTimer;
+    public int cardTimer;
+
+    private GamePhase currentPhase;
+    private float timer;
+    private List<GameObject> players;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if (timer <= 0)
+        {
+            currentPhase++;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+	}
+}
---
+++ Card Clash/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private int lives;
+    private int damage;
+    private int mana;
+    private int maxMana;
+    private GameObject myCharacter;
+    private DeckScript myDeck;
+    private List<GameObject> myHand;
+    private List<GameObject> myGraveyard;
+
+    public int Lives
+    {
+        get { return lives; }
+        set { lives = value; }
+    }
+
+    public int Damage
+    {
+        get { return damage; }
+        set
+        {
+            if (value < 0)
+            {
+                damage = 0;
+            }
+            else if (value > 999)
+            {
+                damage = 999;
+            }
+            else
+            {
+                damage = value;
+            }
+        }
+    }
+
+    public GameObject Character
+    {
+        get { return myCharacter; }
+        set { myCharacter = value; }
+    }
+
+    public DeckScript Deck
+    {
+        get { return myDeck; }
+        set { myDeck = value; }
+    }
+
+    public List<GameObject> Hand
+    {
+        get { return myHand; }
+        set { myHand = value; }
+    }
+
+    public List<GameObject> Graveyard
+    {
+        get { return myGraveyard; }
+        set { myGraveyard = value; }
+    }
+}
