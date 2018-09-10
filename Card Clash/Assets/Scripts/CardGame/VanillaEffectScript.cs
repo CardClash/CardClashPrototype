83b91d2c8c152b2f9a7c74a24f29278c6518665d
Aditya Wirastomo (RIT Student) <aaw9185@ad.rit.edu>
2018-09-10 11:37:04 -0400

    Added Basic Card Effects

---
+++ Card Clash/Assets/Scripts/CardGame/VanillaEffectScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanillaEffectScript : MonoBehaviour {
+
+    private FighterScript source;
+    private FighterHealthScript health;
+
+	// Use this for initialization
+	void Start () {
+        source = GameObject.Find("Main Character").GetComponent<FighterScript>();
+        health = GameObject.Find("Main Character").GetComponent<FighterHealthScript>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            health.TakeDamage(10);
+            print("took damage");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            source.playerSpeed = source.playerSpeed + 1;
+            print("speed increased by 1");
+        }
+
+	}
+}
