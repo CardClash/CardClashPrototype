8290d0a826b925edd82067d7ff58746fe87efcb6
PlatFleece <maverick.alpha4@gmail.com>
2018-09-19 10:31:58 -0400

    Framework of Stacking Mechanics

--- Card Clash/Assets/Scripts/CardGame/VanillaEffectScript.cs
+++ Card Clash/Assets/Scripts/CardGame/VanillaEffectScript.cs
@@ -6,11 +6,14 @@ public class VanillaEffectScript : MonoBehaviour {
 
     private FighterScript source;
     private FighterHealthScript health;
+    private Stack stack;
+    private bool active = false;
 
 	// Use this for initialization
 	void Start () {
         source = GameObject.Find("Main Character").GetComponent<FighterScript>();
         health = GameObject.Find("Main Character").GetComponent<FighterHealthScript>();
+        stack = new Stack();
     }
 	
 	// Update is called once per frame
@@ -18,8 +21,11 @@ public class VanillaEffectScript : MonoBehaviour {
 		
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
+
             health.TakeDamage(10);
             print("took damage");
+
+            //stack.Push(takeDamage);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
@@ -28,5 +34,20 @@ public class VanillaEffectScript : MonoBehaviour {
             print("speed increased by 1");
         }
 
+    /*    if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            foreach(Object obj in st)
+            {
+
+            }
+        }
+    */
 	}
+
+  /*  void takeDamage()
+    {
+            health.TakeDamage(10);
+            print("took damage");
+
+    } */
 }
