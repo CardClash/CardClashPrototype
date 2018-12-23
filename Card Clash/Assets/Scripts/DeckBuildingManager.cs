cb01075cdedef9f3d73d14f8142f1e456a8bc67c
cbb1320 <cbb1320@g.rit.edu>
2018-12-22 20:56:23 -0500

    Deckbuilding Scene Updated

--- Card Clash/Assets/Scripts/DeckBuildingManager.cs
+++ Card Clash/Assets/Scripts/DeckBuildingManager.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class DeckBuildingManager : MonoBehaviour {
 
-    public float leftOffset;
-    public float upOffset;
-    public float spacingRight;
-    public float spacingDown;
-    public float buttonHeight;
     public string[] cardNames;
-    public float smallWidth;
     public int upperLimit;
+
+    public GameObject finishedButton;
+    public GameObject dmg10Button;
+    public GameObject speedUpButton;
+    public GameObject healButton;
+    public GameObject teleButton;
+    public GameObject dmg25Button;
+    public GameObject arrowButton;
+
+    private Color highlightedColor;
+
+    public GameObject totalCardText;
+    public GameObject dmg10Text;
+    public GameObject speedUpText;
+    public GameObject healText;
+    public GameObject teleText;
+    public GameObject dmg25Text;
+    public GameObject arrowText;
 
     private int[] cardNums;
     private int total;
@@ -25,44 +39,108 @@ public class DeckBuildingManager : MonoBehaviour {
         {
             cardNums[i] = 0;
         }
+
+        finishedButton.GetComponent<Button>().interactable = false;
+
+        highlightedColor = new Color();
+
+        ColorUtility.TryParseHtmlString("00D1FF", out highlightedColor);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
-
-    private void OnGUI()
-    {
-        for (int i = 0; i < cardNames.Length; i++)
+        #region Add Cards
+        if(total < upperLimit)
         {
-            GUI.Button(new Rect(leftOffset, upOffset + (spacingDown * i), 200, buttonHeight), cardNames[i]);
-            if (cardNums[i] > 0)
+            if(dmg10Button == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Submit"))
             {
-                if (GUI.Button(new Rect(leftOffset + spacingRight, upOffset + (spacingDown * i), smallWidth, buttonHeight), "-"))
-                {
-                    --cardNums[i];
-                    --total;
-                }
+                ++cardNums[0];
+                ++total;
             }
-            GUI.Button(new Rect(leftOffset + (spacingRight * 2), upOffset + (spacingDown * i), smallWidth, buttonHeight), cardNums[i].ToString());
-            if (total < upperLimit)
+            if (speedUpButton == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Submit"))
             {
-                if (GUI.Button(new Rect(leftOffset + (spacingRight * 3), upOffset + (spacingDown * i), smallWidth, buttonHeight), "+"))
-                {
-                    ++cardNums[i];
-                    ++total;
-                }
+                ++cardNums[1];
+                ++total;
+            }
+            if (healButton == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Submit"))
+            {
+                ++cardNums[2];
+                ++total;
+            }
+            if (teleButton == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Submit"))
+            {
+                ++cardNums[3];
+                ++total;
+            }
+            if (dmg25Button == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Submit"))
+            {
+                ++cardNums[4];
+                ++total;
+            }
+            if (arrowButton == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Submit"))
+            {
+                ++cardNums[5];
+                ++total;
             }
         }
+        #endregion
+        #region Remove Cards
+        if (cardNums[0] > 0 && dmg10Button == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Cancel"))
+        {
+            --cardNums[0];
+            --total;
+        }
+        if (cardNums[1] > 0 && speedUpButton == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Cancel"))
+        {
+            --cardNums[1];
+            --total;
+        }
+        if (cardNums[2] > 0 && healButton == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Cancel"))
+        {
+            --cardNums[2];
+            --total;
+        }
+        if (cardNums[3] > 0 && teleButton == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Cancel"))
+        {
+            --cardNums[3];
+            --total;
+        }
+        if (cardNums[4] > 0 && dmg25Button == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Cancel"))
+        {
+            --cardNums[4];
+            --total;
+        }
+        if (cardNums[5] > 0 && arrowButton == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Cancel"))
+        {
+            --cardNums[5];
+            --total;
+        }
+        #endregion
         if (total == upperLimit)
         {
-            if (GUI.Button(new Rect(leftOffset, upOffset + (spacingDown * cardNames.Length + 1), smallWidth, buttonHeight), "Done"))
+            finishedButton.GetComponent<Button>().interactable = true;
+
+            if(finishedButton == EventSystem.current.currentSelectedGameObject && Input.GetButtonDown("Submit"))
             {
                 MyDeckOfCards.deck = cardNums;
+                Debug.Log(cardNums);
                 SceneManager.LoadScene("Network Fight Scene");
             }
         }
+        else
+        {
+            finishedButton.GetComponent<Button>().interactable = false;
+        }
+
+        #region Updating Text
+        totalCardText.GetComponent<Text>().text = "Total Card Count: "  + total.ToString();
+        dmg10Text.GetComponent<Text>().text = cardNums[0].ToString();
+        speedUpText.GetComponent<Text>().text = cardNums[1].ToString();
+        healText.GetComponent<Text>().text = cardNums[2].ToString();
+        teleText.GetComponent<Text>().text = cardNums[3].ToString();
+        dmg25Text.GetComponent<Text>().text = cardNums[4].ToString();
+        arrowText.GetComponent<Text>().text = cardNums[5].ToString();
+        #endregion
     }
 }
